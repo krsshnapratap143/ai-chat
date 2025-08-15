41a29d069eac43aff42d4df78cad92005f04cfc1
krsshna <krsshnayadav@gmail.com>
2025-08-16 04:40:23 +0530

    updated pages.js

--- app/page.js
+++ app/page.js
@@ -1,10 +1,50 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState } from "react";
 
 export default function Home() {
+  const [message, setMessage] = useState("");
+  const [response, setResponse] = useState("");
+  const [streaming, setStreaming] = useState("");
+  const [loading, setLoading] = useState("");
+  const [streamResponse, setStreamResponse] = useState("");
+
+  const handleChat = async () => {
+    setLoading(true)
+    setResponse("")
+    try{
+      const res = await fetch("/api/chat", {
+        method: "POST", 
+        headers: {
+          "content-type" : "application/json"
+        }, 
+        body: JSON.stringify({message})
+      })
+      const data = await res.json()
+      setResponse(data.response)
+    }
+    catch (error) {
+      setResponse("error : " + error.message)
+    }
+    setLoading(false)
+  }
+
   return(
     <div className={styles.page}>
       <h1>getting started with nextjs and ai</h1>
+      <div>
+        <textarea
+        value={message}
+        onChange={(e) => e.target.value}
+        placeholder="enter your awesome message"
+        rows={4}
+        style={{width: "100%", marginBottom: "10px"}}
+        />
+      </div>
+      <div>
+        <button>{loading ? "loading..." : "chat"}</button>
+      </div>
     </div>
   )
 }
